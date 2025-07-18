b005176c3b66a679b56f4df5b89f8a9daaf01a01
samy <ab2gbl003@gmail.com>
2025-07-18 10:27:50 +0100

    database setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,21 +93,22 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-           
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'ecommerce'),
-        'USER': os.environ.get('POSTGRES_USER', 'ecommerceuser'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'ecommercepass'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
 
-  
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB', 'ecommerce'),
+            'USER': os.environ.get('POSTGRES_USER', 'ecommerceuser'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'ecommercepass'),
+            'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+            'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        }
+    }
 
 
 # Password validation
